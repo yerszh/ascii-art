97d5345265265a98443dbcebef5e79815f4bc00a
yerszh <yers.zh@gmail.com>
2023-12-16 16:20:40 +0500

    internal prepare.go

--- internal/prepare.go
+++ internal/prepare.go
@@ -7,29 +7,29 @@ import (
 )
 
 func onlyNewLines(input []string) bool {
-	for _, ch := range input { // Цикл для перебора аргумента для вывода
-		if ch != "" { // Проверка на пустую строку
-			return false // Возвращаем false
+	for _, ch := range input {
+		if ch != "" {
+			return false
 		}
 	}
-	return true // Возвращаем true
+	return true
 }
 
 func PrepareForOutput(banner string, text string) ([]string, []string) {
-	dataBytes, _ := os.ReadFile("assets/" + banner + ".txt") // Считываем содержимое файла в байтовый массив.
+	dataBytes, _ := os.ReadFile("assets/" + banner + ".txt")
 	standardAscii := []string{}
 
-	if banner == "thinkertoy" { // Проверяем, является ли файл "assets/thinkertoy.txt".
-		standardAscii = strings.Split(string(dataBytes), "\r\n") // Разделяем текст по символам перевода строки Windows.
+	if banner == "thinkertoy" {
+		standardAscii = strings.Split(string(dataBytes), "\r\n")
 	} else {
-		standardAscii = strings.Split(string(dataBytes), "\n") // Разделяем текст по обычным символам перевода строки.
+		standardAscii = strings.Split(string(dataBytes), "\n")
 	}
 
-	inputWords := text                                                  // Получаем входные слова для преобразования в ASCII-арт.
-	splittedWords := regexp.MustCompile(`\n|\\n`).Split(inputWords, -1) // Разбиваем входные слова на части, используя регулярное выражение.
+	inputWords := text
+	splittedWords := regexp.MustCompile(`\n|\\n`).Split(inputWords, -1)
 
-	if onlyNewLines(splittedWords) { // Проверяем, состоит ли текст только из символов перевода строки.
-		splittedWords = splittedWords[1:] // Если да, удаляем первый элемент (пустую строку).
+	if onlyNewLines(splittedWords) {
+		splittedWords = splittedWords[1:]
 	}
 
 	return splittedWords, standardAscii
