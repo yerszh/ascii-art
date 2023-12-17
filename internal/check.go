8cdeac0aadafa232ca758a6b0c2f91ba3d49c3ac
yerszh <yers.zh@gmail.com>
2023-12-17 16:20:40 +0500

    internal check.go

--- internal/check.go
+++ internal/check.go
@@ -7,43 +7,40 @@ import (
 	"os"
 )
 
-func ClearFlag(firstArgument string) string { 
-	flag := "" 							 					  
-	for _, char := range firstArgument {									
-		if char != '=' { 
-			flag += string(char) 
-		} else { 
-			break 
+func ClearFlag(firstArgument string) string {
+	flag := ""
+	for _, char := range firstArgument {
+		if char != '=' {
+			flag += string(char)
+		} else {
+			break
 		}
-	} 																											  
-	return flag 
+	}
+	return flag
 }
-
 
 func FileMD5(path string) string {
-	h := md5.New() 
-	f, err := os.Open(path) 
-	if err != nil {     
-		os.Exit(1) 
+	h := md5.New()
+	f, err := os.Open(path)
+	if err != nil {
+		os.Exit(1)
 	}
-	defer f.Close() 
+	defer f.Close()
 
-	_, err = io.Copy(h, f) 
-	if err != nil {    
-		os.Exit(1) 
+	_, err = io.Copy(h, f)
+	if err != nil {
+		fmt.Println("")
+		os.Exit(1)
 	}
-
-	return fmt.Sprintf("%x", h.Sum(nil)) 
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
-
 func CheckForChangeFile(textAsFileName string, banner string) bool {
-	checkHashSumFile := FileMD5(textAsFileName) 
-
-	switch banner { 
-	case "standard": 
-		if "ac85e83127e49ec42487f272d9b9db8b" != checkHashSumFile { 
-			return false 
+	checkHashSumFile := FileMD5(textAsFileName)
+	switch banner {
+	case "standard":
+		if "ac85e83127e49ec42487f272d9b9db8b" != checkHashSumFile {
+			return false
 		}
 	case "thinkertoy":
 		if "86d9947457f6a41a18cb98427e314ff8" != checkHashSumFile {
@@ -54,5 +51,25 @@ func CheckForChangeFile(textAsFileName string, banner string) bool {
 			return false
 		}
 	}
-	return true 
+	return true
+}
+
+func CheckIsAscii(text string) (string, string) {
+	validText := ""
+	for _, r := range text {
+		if r == '\n' || r >= 32 && r <= 126 {
+			validText += string(r)
+		}
+	}
+	if text != "" && validText == "" {
+		return "", "Mask characters from 32 to 126 characters or character 10 are available for input"
+	}
+	return validText, ""
+}
+
+func CheckIsBanner(banner string) bool {
+	if banner != "standard" && banner != "shadow" && banner != "thinkertoy" {
+		return false
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -8,15 +8,17 @@ import (
 )
 
 func main() {
-	if len(os.Args) >= 2 { 
-		flag := internal.ClearFlag(os.Args[1]) 
+if len(os.Args) >= 2 {
+	flag := internal.ClearFlag(os.Args[1]) 
 
-		switch flag { 
-		
-		default: 
-			option.Fs()
-		}
-	} else {
-		fmt.Println("Wrong number of arguments") 
+	switch flag { 
+
+	
+	
+	default: 
+		option.Fs()
 	}
+} else {
+	fmt.Println("Wrong number of arguments") 
+}
 }
